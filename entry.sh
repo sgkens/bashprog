3ebc07e96e97e1eeefc606a70f975f139e7a23c3
sgkens <sgkens@gmail.com>
2025-05-22 09:37:26 +1000

    🔧 chore: Update entry file
    🧳 Notes:
    
      - UPDATE: entry.sh module entry file
      - Add VERSION text file

--- entry.sh
+++ entry.sh
@@ -16,13 +16,14 @@
 #        @@@@@@@@@@@@@@@@@@@@@@       
 #        BASH PROG @@@#@@@ @@@#@                                  
 #                   ...:::---==+      
-# =======================================================================                                                                       
-
+# ==============================   
 # Main Entry Point
 # This script is the main entry point for the BashProg library.
+# =======================================================================                                                                       
 
 # initialize the BPROG_HOME variable
 : "${BPROG_HOME:=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )}"
+: "${BPROG_VERSION:="v$(cat ${BPROG_HOME}/VERSION)"}"
 
 # Set the default debug mode
 BPROG_DEBUG=1 # default is 0, set with bprog_set_debug 1 or bprog with --debug flag
