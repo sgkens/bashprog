64562530b485853aaf81dde25dec8de34e1224ba
sgkens <sgkens@gmail.com>
2025-05-22 09:34:17 +1000

    🔧 chore: Adding tests(wip)

--- tools/commit-helper.ps1
+++ tools/commit-helper.ps1
@@ -3,22 +3,30 @@
   use to create large detailed commits
 #>
 $Params = @{
-  Type             = "chore";
-  #Scope = "";
-  Description      = "Implemented additional module logic";
+  Type             = "wip";
+  Scope            = "main command";
+  Description      = "logic and switches";
   GitUser          = "sgkens";
   #GitGroup         = "phellams";
   Notes            = @(
-    "ADD: function bashprog as main command file",
-    "ADD: entry.sh as main entry point simulate module loading",
-    "UPDATE: bkvp to call the new clstring function",
-    "UPDATE: help function to output examples for commands"
+    "ADD: Param switch to to switch modes",
+    "ADD: 2 additional modes list and rewrite",
+    "ADD: Param switch to enable optional message output for bars and spinners",
+    "ADD: Param switch to enable/disable debug logging",
+    "ADD: Param switches for Color support for bars and spinners base 16 colors",
+    "ADD: switch to enable/disable demo mode for bars and spinners",
+    "ADD: Param switch to call the clearlines function to rewrite lines in the console",
+    "ADD: Update help function call to include updated help function call",
+    "ADD: Param switch to list all|bars|spinners themes",
+    "ADD: Default text for message for spinners and bars, overide default message by entering a empty message string or -nm|--nomessage",
+    "UPDATE: show help to now output error/warning notifications to under help output",
+    "UPDATE: Debug output to reflect switch state and values for better debugging"
   );
 
   # FeatureAdditions = @(
   #   "Output command help --help"
   # );
-  #semver = "none";
+  semver = "patch";
   # BugFixes = @(
   #   "- Emojis that were output to the console within a PSObject disrupt the alignment of the object, throwing other field alignments off.",
   #   "- FIX: `Select-Object` to manually output in order so that emoji is last; this fixed the alignment."
