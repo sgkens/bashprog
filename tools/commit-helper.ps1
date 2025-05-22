43f9b814e1e8896864d8105c3c9f898f5c43297e
sgkens <sgkens@gmail.com>
2025-05-22 11:19:04 +1000

    🔧 chore(tools): Update scripts

--- tools/commit-helper.ps1
+++ tools/commit-helper.ps1
@@ -3,22 +3,20 @@
   use to create large detailed commits
 #>
 $Params = @{
-  Type             = "chore";
-  #Scope = "";
-  Description      = "Implemented additional module logic";
+  Type             = "docs";
+  Scope            = "bar/spinner generation script";
+  Description      = "update bar/spinner example generation scripts";
   GitUser          = "sgkens";
   #GitGroup         = "phellams";
   Notes            = @(
-    "ADD: function bashprog as main command file",
-    "ADD: entry.sh as main entry point simulate module loading",
-    "UPDATE: bkvp to call the new clstring function",
-    "UPDATE: help function to output examples for commands"
-  );
+    "UDPATE: bash script to output progress bars and spinners example svgs and markdown code"
+
+    );
 
   # FeatureAdditions = @(
   #   "Output command help --help"
   # );
-  #semver = "none";
+  #semver = "patch";
   # BugFixes = @(
   #   "- Emojis that were output to the console within a PSObject disrupt the alignment of the object, throwing other field alignments off.",
   #   "- FIX: `Select-Object` to manually output in order so that emoji is last; this fixed the alignment."
